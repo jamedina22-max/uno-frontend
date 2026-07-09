61b36cc65fe55a7659cfe15a4bff93bbef82cac5
jamedina22-max <jamedina.22@est.ucab.edu.ve>
2026-07-09 09:26:44 -0400

    sokecket

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const ws = new WebSocket(`ws://${window.location.host}`);
+const ws = new WebSocket('wss://uno-bakend.onrender.com');
 
 // DOM Elements - Screens
 const lobbyScreen = document.getElementById('lobby');
